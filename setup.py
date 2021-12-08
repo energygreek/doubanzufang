8fbd116ccf128a1db33f481b88deae3817b62117
Heyman <greek_soon@hotmail.com>
2021-12-08 14:29:38 +0800

    Create README.md

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name         = 'project',
-    version      = '1.0',
+    version      = '1.1',
     packages     = find_packages(),
     entry_points = {'scrapy': ['settings = doubanzufang.settings']},
 )
